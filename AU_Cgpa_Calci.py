25f0e1c1114f079b1f743fd1e7a930e29c35f1ce
Praveenias <43996168+Praveenias@users.noreply.github.com>
2020-08-02 17:36:40 +0530

    Update AU_Cgpa_Calci.py

--- AU_Cgpa_Calci.py
+++ AU_Cgpa_Calci.py
@@ -1,47 +1,45 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-print("\t\t****CGPA CALCULATOR*****")
-a = input("Enter the department cse/ece/mech:")
-def cgpa(dept_paper,dept_credit):
+print('******CGPA CALCULATOR*****')
+a = input("Enter the department:")
+
+def cgpa(c):
     try:
+        l = []
+        k = 0
+        cr= 0
         grade = {'o':10,'a+':9,'a':8,'b+':7,'b':6,'ra':0}
         b = int(input("ENTER YOUR SEMESTER:"))
-        k = 0
-        li = []
-        cr = 0
-        for i in range(len(dept_paper[b-1])):
-            print("ENTER YOUR GRADE ON",dept_paper[b-1][i])
-            w = input()
-            li.append(grade[w])
-            if w =='' or w not in grade:
-                print("PLEASE,ENTER VALID INPUT")
-                break
-                return
-            elif w =='ra':
-                pass
-            else:
-                
-                k+= int(grade[w]*dept_credit[b-1][i])
-                cr+= dept_credit[b-1][i]
-        print("GPA FOR THE {} SEMESTER IS {}".format(b,k/cr))
+        if b:
+            h = b-1
+            for i in range(h*8,b*8):
+                print("Enter marks for",c['SUB_CODE'][i],"-",c['SUBJECT_NAME'][i],":")
+                w = input()
+                if w not in grade :
+                    print("Enter Valid Input")
+                    return
+                else:
+                    l.append(grade[w])
+                    k+= int(grade[w]*c['CREDITS'][i])
+                    cr+= c['CREDITS'][i]
+            print("GPA FOR THE {} SEMESTER IS {}".format(b,k/cr))
+            plt.bar(c['SUB_CODE'][h*8:b*8],l)
+            plt.xlabel("Subjects")
+            plt.ylabel("Marks out of 10")
     except:
-        print("\t\t**&**TRY ANOTHER TIME**&**")
-    xaxis = np.arange(len(cse_paper[b]))
-    plt.xticks(xaxis,cse_paper[b-1])
-    plt.bar(xaxis,li)
-
-if a =='cse':
-    cse_paper =[['MATHS-1','ENGINEERING CHEMISTRY','ENGINEERING PHYSICS','ENGINEERING GRAPHICS','PROBLEM SOLVING THROUGH PYTHON','ENGLISH-1','PYTHON LABROTARY','PHYSICS AND CHEMISTRY LABORATATY'],
-               ['ENGLISH-2','PHYSICS=2','EVS','MATHS-2','PROGRAMING IN C','BASIC ELECTRIC ENGINEERING','ENGINEERNG PRACTISE LABAROTARY','C LABORARTRY'],
-               ['DISCREATE MATHEMATICS','DIGITAL PRINCIPAL AND SYSTEM DEISGN','DATA STRUCTURE','OOP LAUNGUAGE','COMMUNUCATION ENGINEERING','DATA STRUCTURE LAB','OOP LAB','DIGITAL SYSTEM LAB','INTERPERSONNEL SKILL LAB'],
-               ['PQT','COMPUTER ARCHITECTURE','DBMS','DAA','OS','SOFTWARE ENGINEERING','DBMS LAB','OOPS LAB','ADVANCED WRITING AND READING LAB'],[]]
-    cse_credit = [[4,3,3,4,3,4,2,2],[4,3,3,4,3,3,2,2],[4,4,3,3,3,2,2,2,1],[4,3,3,3,3,3,2,2,1]]
-    cgpa(cse_paper,cse_credit)
-    
-elif a =='ece':
-    pass
-elif a =='mech':
-    pass
+        print('**Sorry,Try another time***')
+        
+                
+if a == "cse":
+    c = pd.read_excel(r"C:\Users\PRAVEEN\Documents\PYthon_Praveen\CSE.xlsx",sheet_name='CSE')
+    cgpa(c)
+if a== "ece":
+    c = pd.read_excel(r"C:\Users\PRAVEEN\Documents\PYthon_Praveen\CSE.xlsx",sheet_name='ECE')
+    cgpa(c)
+if a=="mech":
+    c = pd.read_excel(r"C:\Users\PRAVEEN\Documents\PYthon_Praveen\CSE.xlsx",sheet_name='MECH')
+    cgpa(c)
 else:
-    print("PLEASE,ENTER ONLY THOSE 3 DEPARTMENT")
+    print("Enter only cse/ece/mech Departments")
+    
